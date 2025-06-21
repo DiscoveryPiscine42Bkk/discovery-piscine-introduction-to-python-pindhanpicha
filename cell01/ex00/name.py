7aba5384f4740d4625a05c5bfd61fbcfd3ac335b
pindhanpicha <pindharapak@gmail.com>
2025-06-21 09:11:17 +0000

    add file

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,4 +1,3 @@
 First_name = "Dhanpicha"
 Last_name = "Dharapk"
-
 print(First_name, Last_name)
---
+++ cell01/name.py
@@ -0,0 +1,4 @@
+first_name = "Dhanpicha" 
+last_name = "Dharapak"
+whole_name = first_name + " " + last_name 
+print(whole_name)
