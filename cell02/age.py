c7f265776bdfa18148122c222628f91a502f5774
pindhanpicha <pindharapak@gmail.com>
2025-06-22 03:58:10 +0000

    add new file

--- cell02/age.py
+++
@@ -1,3 +0,0 @@
-Age = 19 
-my_age = int(Age) + 42
-print(my_age)
---
+++ cell02/iszero.py
@@ -0,0 +1,6 @@
+#cell2 exercise 00 am I null
+number = int(input("Enter a number: "))
+if number == 0:
+    print ("The number is equal to zero ")
+else:
+    print("this number is different from zero.")
