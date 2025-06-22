f79c248434d1b28555bb119d1dbda11e9fe3708d
pindhanpicha <pindharapak@gmail.com>
2025-06-22 04:28:32 +0000

    add password.py

---
+++ cell02/password.py
@@ -0,0 +1,8 @@
+#Cell02Ex02 Password
+my_password = "1234"
+print ("Enter your password")
+password = input()
+if password == my_password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
