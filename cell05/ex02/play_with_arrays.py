6f4dce533f98609b9eee59636f63c04fedc93120
pindhanpicha <pindharapak@gmail.com>
2025-06-23 10:13:50 +0000

    fixCell05Ex02

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,8 +1,6 @@
 #!/urs/bin/env3 python3 
-original_array = [2, 8, 9, 48, 8,22 -12,2]
+original_array = [2, 8, 9, 48, 8, 22, -12,2]
 filtered_array = [num for num in original_array if num > 5]
 new_array = [num+2 for num in filtered_array]
-print(", ".join(str(num) for num in filtered_array))
+print(original_array)
 print(new_array)
-
-fix please
