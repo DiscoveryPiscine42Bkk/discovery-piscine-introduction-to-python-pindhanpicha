4c05554267490c9350bafe0dc8f9572b3503b6bb
pindhanpicha <pindharapak@gmail.com>
2025-06-24 05:29:59 +0000

    ad this file

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,6 +1,8 @@
 age = int(input("please tell me your age: "))
 print("You are currently",age,"years old.")
-while age:
-    print("In 10 years, you'll be", age + 10 "years old.")
-    print("In 20 years, you'll be", age + 20 "years old.")
-    print("In 30 years, you'll be", age + 30 "years old.")
+while age:5
+    print("In 10 years, you'll be", age + 10, "years old.")
+    print("In 20 years, you'll be", age + 20, "years old.")
+    print("In 30 years, you'll be", age + 30, "years old.")
+    break
+
