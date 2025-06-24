79511a0bbe8d237d6d25a91d37f82c3dda704198
pindhanpicha <pindharapak@gmail.com>
2025-06-24 03:40:44 +0000

    add this file

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-n1 = input(input("type a number: "))
+n1 = int(input("type a number: "))
 n2 = int(input("type a number: "))
 x = 0
-a = int(n1)
-b = int(n2)
 result = int(n1* n2)
 print(result)
 if result > x:
