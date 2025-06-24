69da88c98bf9265181fedc3096ad3cdc9f0a607c
pindhanpicha <pindharapak@gmail.com>
2025-06-24 03:53:47 +0000

    add this file

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -3,4 +3,4 @@ print("You are currently",age,"years old.")
 while age:
     print("In 10 years, you'll be", age + 10 "years old.")
     print("In 20 years, you'll be", age + 20 "years old.")
-    print("In 20 years, you'll be", age + 30 "years old.")
+    print("In 30 years, you'll be", age + 30 "years old.")
